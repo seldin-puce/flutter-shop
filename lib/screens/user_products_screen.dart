15550c7a22d5b9d3390e147146711ae94007264c
seldin-puce <puceseldin@gmail.com>
2020-12-25 20:10:15 +0100

    change lap

--- lib/screens/user_products_screen.dart
+++ lib/screens/user_products_screen.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_complete_guide/providers/products.dart';
+import 'package:flutter_complete_guide/widgets/app_drawer.dart';
 import 'package:flutter_complete_guide/widgets/user_product_item.dart';
 import 'package:provider/provider.dart';
 
 class UserProductsScreen extends StatelessWidget {
   static const routeName = '/user-products';
+
   @override
   Widget build(BuildContext context) {
     var productsData = Provider.of<Products>(context);
@@ -15,11 +17,17 @@ class UserProductsScreen extends StatelessWidget {
           IconButton(icon: Icon(Icons.add), onPressed: () {}),
         ],
       ),
+      drawer: AppDrawer(),
       body: Padding(
         padding: EdgeInsets.all(8),
         child: ListView.builder(
-          itemBuilder: (_, i) => UserProductItem(
-              productsData.items[i].title, productsData.items[i].imageUrl),
+          itemBuilder: (_, i) => Column(
+            children: [
+              UserProductItem(
+                  productsData.items[i].title, productsData.items[i].imageUrl),
+              Divider(),
+            ],
+          ),
           itemCount: productsData.items.length,
         ),
       ),
